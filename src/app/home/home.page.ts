cf2d1d0e1e9007af07a2b576107a6e5b3f2e92ea
andreavecino <andreavecino94@gmail.com>
2024-07-11 20:19:55 -0500

    textos de la descripcion

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -20,14 +20,14 @@ export class HomePage {
       icon: "diamond-outline",
       avatar: "https://as1.ftcdn.net/v2/jpg/00/89/61/72/1000_F_89617216_hXm7SwZCOP1KaWVBKl7OnTnxBQOzst8Q.jpg",
       image: "https://elcomercio.pe/resizer/myMotgdrOB9o-s2Fsp-6mmQdQEw=/580x330/smart/filters:format(jpeg):quality(75)/arc-anglerfish-arc2-prod-elcomercio.s3.amazonaws.com/public/23A55HEUUBEVPPRXJYG7BZQWQY.jpg",
-      description: "Texto de description"
+      description: "Texto de description 2 hola hola"
     },
     {
       title: "Titulo de slide 3",
       icon: "diamond-outline",
       avatar: "https://as1.ftcdn.net/v2/jpg/00/89/61/72/1000_F_89617216_hXm7SwZCOP1KaWVBKl7OnTnxBQOzst8Q.jpg",
       image: "https://elcomercio.pe/resizer/myMotgdrOB9o-s2Fsp-6mmQdQEw=/580x330/smart/filters:format(jpeg):quality(75)/arc-anglerfish-arc2-prod-elcomercio.s3.amazonaws.com/public/23A55HEUUBEVPPRXJYG7BZQWQY.jpg",
-      description: "Texto de description"
+      description: "Texto de description 3 hola ultimo slide"
     }
   ]
 
